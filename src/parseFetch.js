993a4a0611991901cf94ccf6de22d91a517125dd
Allain Lalonde <allain.lalonde@gmail.com>
2019-04-15 20:03:01 -0400

    starting parse

--- src/parseFetch.js
+++ src/parseFetch.js
@@ -1,14 +1,40 @@
+const mapToJson = m =>
+  m.size
+    ? [...m.entries()].reduce(
+      (map, [key, value]) => ({ ...map, [key]: value }),
+      {}
+    )
+    : undefined
+
 export default function parseFetch (content) {
-  const { method, url } = extractRequestLine(content)
-  const headers = extractHeaders(content)
+  const { method, path } = extractRequestLine(content)
+  const headersMap = extractHeaders(content)
+  const contentType = headersMap.get('Content-Type')
+
+  const body = extractBody(content, contentType)
+
+  let url
+  if (path[0] === '/') {
+    // no host given in request line, looking in headers
+    const host = headersMap.get('Host')
+    headersMap.delete('Host')
+    url = 'http://' + host + path
+  } else {
+    url = path
+  }
+
+  const optionsMap = new Map()
+  if (body) optionsMap.set('body', body)
+  if (method && !method.match(/^get$/i)) optionsMap.set('method', method)
+
+  const headers = mapToJson(headersMap)
+  if (headers) optionsMap.set('headers', headers)
+  const options = mapToJson(optionsMap)
 
   return JSON.parse(
     JSON.stringify({
       url,
-      options: {
-        method,
-        headers
-      }
+      options
     })
   )
 }
@@ -20,31 +46,35 @@ function extractRequestLine (content) {
 
   return {
     method: requestLineParts[1],
-    url: requestLineParts[2],
+    path: requestLineParts[2],
     httpVersion: requestLineParts[4]
   }
 }
 
-function extractHeaders (content) {
-  const head = content.substr(
-    0,
-    Math.max(
-      content.indexOf('\n\n'),
-      content.indexOf('\r\n\r\n'),
-      content.length
-    )
-  )
-  const headerBlock = head.replace(/^\s+.*(\r?\n)/, '')
-  if (!headerBlock) return
+function extractBody (content, contentType) {
+  const contentParts = content.split(/\n\n|\r\n\r\n/, 2)
+  if (contentParts.length !== 2) return
+  const rawBody = contentParts.length === 2 ? contentParts[1] : content
 
-  let headerCount = 0
-  const headers = {}
-  for (let line of headerBlock.split(/\r?\n/g)) {
-    const lineParts = /^\s+([^:]+):\s*(.*)\s*$/.exec(line)
-    if (lineParts) {
-      headers[lineParts[1]] = lineParts[2]
-      headerCount++
+  const shouldTrimBody = contentType && contentType.match(/^application\/json/)
+
+  return shouldTrimBody ? rawBody.trim() : rawBody
+}
+
+function extractHeaders (content) {
+  const contentParts = content.split(/\n\n|\r\n\r\n/, 2)
+  const head = contentParts.length === 2 ? contentParts[0] : content
+
+  const headers = new Map()
+
+  const headerBlock = head.replace(/^\s+.*(\r?\n)/, '')
+  if (headerBlock) {
+    for (let line of headerBlock.split(/\r?\n/g)) {
+      const lineParts = /^\s+([^:]+):\s*(.*)\s*$/.exec(line)
+      if (lineParts) {
+        headers.set(lineParts[1], lineParts[2])
+      }
     }
   }
-  return headerCount ? headers : undefined
+  return headers
 }
--- src/parseFetch.test.js
+++ src/parseFetch.test.js
@@ -4,10 +4,17 @@ describe('parseFetch', () => {
   it('can parse simplest fetch', () => {
     const parsed = parseFetch('GET http://testing.com')
     expect(parsed).toEqual({
-      url: 'http://testing.com',
-      options: {
-        method: 'GET'
-      }
+      url: 'http://testing.com'
+    })
+  })
+
+  it('constructs full url from headers', () => {
+    const parsed = parseFetch(`
+      GET /
+      Host: testing.com
+    `)
+    expect(parsed).toEqual({
+      url: 'http://testing.com/'
     })
   })
 
@@ -20,11 +27,30 @@ describe('parseFetch', () => {
     expect(parsed).toEqual({
       url: 'http://testing.com',
       options: {
-        method: 'GET',
         headers: {
           Foo: 'Bar',
           Fuzz: 'Bizz'
         }
+      }
+    })
+  })
+
+  it('trims whitespace on JSON requests', () => {
+    const parsed = parseFetch(`
+      POST http://testing.com
+      Content-Type: application/json
+
+      ${JSON.stringify({ a: 10, b: { c: true } })}
+    `)
+
+    expect(parsed).toEqual({
+      url: 'http://testing.com',
+      options: {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: '{"a":10,"b":{"c":true}}'
       }
     })
   })
