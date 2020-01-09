4ba68b7186e1fd121a103425aa62123a92ba6ceb
Allain Lalonde <allain.lalonde@elastalink.com>
2020-01-09 16:49:19 -0500

    adding support for JSON values in literals

--- src/fetchLiteral.js
+++ src/fetchLiteral.js
@@ -1,23 +1,38 @@
 import parseFetch from './parseFetch.js'
 
-const interpolate = (template, substitutions) =>
-  typeof template === 'string'
-    ? template // no-op
-    : String.raw(template, ...substitutions)
+function fetchLiteral(template, ...subs) {
+  return typeof template === 'function'
+    ? buildFetcherLiteral(template)
+    : performFetch(interpolate(template, subs), global.fetch)
+}
 
-function performFetch (requestSpec, fetch) {
+function buildFetcherLiteral(fetch) {
+  return (template, ...subs) =>
+    performFetch(interpolate(template, subs), fetch)
+}
+
+function performFetch(requestSpec, fetch) {
   const parsed = parseFetch(requestSpec)
   return fetch(parsed.url, parsed.options)
 }
 
-function fetchLiteral (template, ...substitutions) {
-  return typeof template === 'function'
-    ? buildFetcherLiteral(template)
-    : performFetch(interpolate(template, substitutions), global.fetch)
+const interpolate = (template, subs) => {
+  return typeof template === 'string'
+    ? template // no-op
+    : String.raw(template, ...subs.map(stringify))
 }
 
-function buildFetcherLiteral (fetch) {
-  return (template, ...substitutions) =>
-    performFetch(interpolate(template, substitutions), fetch)
+function stringify(x) {
+  switch (typeof (x)) {
+    case 'object':
+      if (x == null) {
+        throw new Error(`invalid value for substitution: ${x}`)
+      }
+      break;
+    case 'function':
+      throw new Error(`invalid value for substitution: ${x}`)
+  }
+  return JSON.stringify(x)
 }
+
 export default fetchLiteral
--- src/fetchLiteral.test.js
+++ src/fetchLiteral.test.js
@@ -18,9 +18,11 @@ describe('fetchLiteral', () => {
   it('can accept a string as first param when using factory', async () => {
     const fetch = jest.fn(async () => true)
 
-    const result = fetchLiteral(fetch)(`
+    const ftl = fetchLiteral(fetch)
+
+    const result = ftl`
       GET https://httpbin.org/get
-      Accept: application/json`)
+      Accept: application/json`
     expect(result).toBeInstanceOf(Promise)
     expect(fetch).toHaveBeenCalledWith('https://httpbin.org/get', {
       headers: { Accept: 'application/json' }
@@ -31,10 +33,10 @@ describe('fetchLiteral', () => {
     const oldFetch = global.fetch
     try {
       global.fetch = jest.fn(async () => true)
-
-      const result = fetchLiteral(`
+      const result = fetchLiteral`
         GET https://httpbin.org/get
-        Accept: application/json`)
+        Accept: application/json`
+
       expect(result).toBeInstanceOf(Promise)
       expect(global.fetch).toHaveBeenCalledWith('https://httpbin.org/get', {
         headers: { Accept: 'application/json' }
@@ -42,6 +44,30 @@ describe('fetchLiteral', () => {
     } finally {
       global.fetch = oldFetch
     }
+  })
+
+  it('interpolates objects as JSON', () => {
+    const fetch = jest.fn(async () => true)
+
+    const payload = { a: 1, b: ["c"], d: null }
+    const ftl = fetchLiteral(fetch)
+
+    const result = ftl`
+      POST https://httpbin.org/post
+      Content-Type: application/json
+      Accept: application/json
+      
+      ${payload}`
+
+    expect(result).toBeInstanceOf(Promise)
+    expect(fetch).toHaveBeenCalledWith('https://httpbin.org/post', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(payload),
+      method: 'POST'
+    })
   })
 
   it('uses global fetch if called without function first', () => {
